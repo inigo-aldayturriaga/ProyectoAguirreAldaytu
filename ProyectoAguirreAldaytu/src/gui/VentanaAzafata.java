3bb500c85d967bb00e9252329794da592ecb3c01
aguirrelander <63808419+aguirrelander@users.noreply.github.com>
2020-12-06 23:27:57 +0100

    Ventanas nuevas 2

--- ProyectoAguirreAldaytu/src/gui/VentanaAzafata.java
+++ ProyectoAguirreAldaytu/src/gui/VentanaAzafata.java
@@ -1,7 +1,9 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+
 
 public class VentanaAzafata extends JFrame {
 	JLabel lnombre;
@@ -14,7 +16,52 @@ public class VentanaAzafata extends JFrame {
     JSpinner spinDni;
     JLabel lresidencia;
     JTextField tResidencia;
-   
+    JButton crear;
+    JButton cancelar;
+    
+    public VentanaAzafata() {
+    	lnombre = new JLabel("Nombre");
+		tnombre = new JTextField(20);
+		lapellido = new JLabel("Apellido");
+		tapellido = new JTextField(20);
+		ledad = new JLabel("Edad");
+		spinEdad = new JSpinner(new SpinnerNumberModel());
+		ldni = new JLabel("DNI");
+		spinDni = new JSpinner(new SpinnerNumberModel());
+		lresidencia = new JLabel("Lugar de residencia");
+		tResidencia = new JTextField(20);		
+		crear = new JButton("Crear");
+		cancelar = new JButton("Cancelar");
+		
+		cancelar.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();				
+			}
+		});
+		
+		this.setLayout(new GridLayout(5, 2));
+		//añadimos las etiquetas, spinners y combobox a la ventana
+		add(lnombre);
+		add(tnombre);
+		add(lapellido);
+		add(tapellido);
+		add(ledad);
+		add(spinEdad);
+		add(ldni);
+		add(spinDni);
+		add(lresidencia);
+		add(tResidencia);
+		add(crear);
+		add(cancelar);
+		
+		
+		//personalizamos la ventana con el titulo, dimensiones,distribucion...y la hacemos visible y se cerrará la ventana si queremos salir de ella
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Añadir nueva azafata");
+		pack();
+		setVisible(true);
+    }
 	
 }
 
