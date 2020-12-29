98346fd23299630044fcf7b0056b7419967dd7ba
msald <msald@10.166.62.114>
2020-12-29 17:17:39 +0100

    VentanaUsuario

---
+++ ProyectoAguirreAldaytu/src/gui/VentanaUsuario.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class VentanaUsuario {
+	private JTable tabla;
+	private DefaultTableModel modelo;
+	
+	public VentanaUsuario () {
+		
+	}
+
+}
